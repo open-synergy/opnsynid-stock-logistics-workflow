4e3a45a815e7df8dcb1e46aa3078e91d621dd817
Andhitia Rama <andhitia.r@gmail.com>
2017-03-18 17:12:20 +0700

    [ADD][8.0] stock_picking_back2draft_policy

---
+++ stock_picking_back2draft_policy/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Picking Back To Draft Policy",
+    "version": "8.0.1.1.0",
+    "category": "Stock Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock_picking_policy",
+        "stock_picking_back2draft",
+    ],
+    "data": [
+        "views/stock_picking_type_views.xml",
+        "views/stock_picking_views.xml",
+    ],
+    "auto_install": True,
+}
---
+++ stock_picking_back2draft_policy/models/stock_picking_type.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    back2draft_group_ids = fields.Many2many(
+        string="Allowed to Back To Draft",
+        comodel_name="res.groups",
+        rel="rel_picking_back2draft",
+        col1="type_id",
+        col2="group_id",
+    )
