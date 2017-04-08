72e27f555f28bb32a2066912b72628bc50211920
Andhitia Rama <andhitia.r@gmail.com>
2017-04-08 09:13:54 +0700

    [ADD][8.0] stock_push_rule_picking_type

---
+++ stock_push_rule_picking_type/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Push Rule Filter by Picking Type",
+    "version": "8.0.1.0.0",
+    "category": "Stock Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock_push_rule_extend",
+    ],
+    "data": [
+        "views/stock_location_path_views.xml",
+    ],
+}
---
+++ stock_push_rule_picking_type/models/stock_location_path.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class StockLocationPath(models.Model):
+    _inherit = "stock.location.path"
+
+    picking_type_ids = fields.Many2many(
+        string="Picking Type",
+        comodel_name="stock.picking.type",
+        relation="rel_push_rule_2_type",
+        col1="push_id",
+        col2="picking_type_id",
+    )
