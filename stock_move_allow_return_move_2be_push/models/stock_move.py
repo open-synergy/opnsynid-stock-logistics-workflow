2c8ed006f5a0805eb2bb46e737200e2651c9557b
Andhitia Rama <andhitia.r@gmail.com>
2017-04-08 09:49:59 +0700

    [ADD][8.0] stock_move_allow_return_move_2be_push

---
+++ stock_move_allow_return_move_2be_push/models/stock_move.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    @api.multi
+    def _prepare_allowed_domain(self):
+        self.ensure_one()
+        domains = super(StockMove, self)._prapare_allowed_domain()
+        for domain in domains:
+            if domain[1] == "origin_returned_move_id":
+                domains.remove(domain)
+        return domains
