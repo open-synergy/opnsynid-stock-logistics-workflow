ae9ec0fdd350561928c5ba84385b5067a93a89ed
Andhitia Rama <andhitia.r@gmail.com>
2017-05-22 07:49:28 +0700

    [8.0.1.0.0][stock_pull_rule_extend]

---
+++ stock_pull_rule_extend/models/procurement_order_monkeypatch.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+from openerp.addons.stock import procurement
+
+
+@api.model
+def _search_suitable_rule(self, procurement, domain):
+    domain = procurement._prepare_init_domain(domain)
+    rules = procurement._find_procurement_pull_rules(domain)
+    if not rules:
+        rules = procurement._find_product_pull_rules(domain)
+        if not rules:
+            if procurement.warehouse_id:
+                rules = procurement._find_warehouse_pull_rules(domain)
+            if not rules:
+                rules = procurement._find_global_pull_rules(domain)
+    return rules
+
+
+class ProcurementOrderMonkeypatch(models.TransientModel):
+    _name = "procurement.order.monkeypatch"
+    _description = "Procurement Order Monkeypatch"
+
+    def _register_hook(self, cr):
+        procurement._search_suitable_rule = _search_suitable_rule
+        return super(ProcurementOrderMonkeypatch, self)._register_hook(cr)
