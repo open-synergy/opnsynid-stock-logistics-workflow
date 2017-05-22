66c7e1f0baffc3c760f008ab80957df3f307bc15
Andhitia Rama <andhitia.r@gmail.com>
2017-05-22 08:47:36 +0700

    [8.0.1.0.0][stock_pull_rule_picking_type]

---
+++ stock_pull_rule_picking_type/models/procurement_rule.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class ProcurementRule(models.Model):
+    _inherit = "procurement.rule"
+
+    picking_type_ids = fields.Many2many(
+        string="Picking Type",
+        comodel_name="stock.picking.type",
+        relation="rel_pull_rule_2_type",
+        col1="pullid",
+        col2="picking_type_id",
+    )
