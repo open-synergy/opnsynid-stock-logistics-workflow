7a6b1a2d496ba387e1153f9e2266dc3cdaca0846
Andhitia Rama <andhitia.r@gmail.com>
2017-10-05 17:32:03 +0700

    stock_inventory_backdating

---
+++ stock_inventory_backdating/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Inventory Backdating",
+    "version": "8.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock_move_backdating",
+    ],
+    "data": [
+        "views/stock_inventory_views.xml",
+    ],
+}
