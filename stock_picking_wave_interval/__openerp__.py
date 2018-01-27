6ac1dbeac2f973f82c9ff9cfa7feedd8a46107d1
Andhitia Rama <andhitia.r@gmail.com>
2018-01-27 11:04:19 +0700

    [8.0.1.0.0] stock_picking_wave_interval

---
+++ stock_picking_wave_interval/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Picking Wave Interval",
+    "version": "8.0.1.0.0",
+    "summary": "Add start date and end date on picking wave",
+    "category": "Stock Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock_picking_wave",
+    ],
+    "data": [
+        "views/stock_picking_wave_view.xml",
+    ],
+}
