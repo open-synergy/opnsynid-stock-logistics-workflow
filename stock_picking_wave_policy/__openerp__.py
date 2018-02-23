fcf8adf5ab028ddf52aaf5e61d7198c356a85222
mike <viriyananda.michael@gmail.com>
2018-02-23 16:52:11 +0700

    [ADD] stock_picking_wave_policy

---
+++ stock_picking_wave_policy/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Picking Wave Policy",
+    "version": "8.0.1.0.0",
+    "category": "Stock Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock_picking_wave_type",
+    ],
+    "data": [
+        "views/stock_picking_wave_type_view.xml",
+        "views/stock_picking_wave_view.xml",
+    ],
+}
---
+++ stock_picking_wave_policy/models/stock_picking_wave_type.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class StockPickingWaveType(models.Model):
+    _inherit = "stock.picking_wave_type"
+
+    confirm_group_ids = fields.Many2many(
+        string="Allowed to Confirm",
+        comodel_name="res.groups",
+        rel="rel_stock_picking_wave_confirm",
+        col1="type_id",
+        col2="group_id",
+    )
+
+    done_group_ids = fields.Many2many(
+        string="Allowed to Done",
+        comodel_name="res.groups",
+        rel="rel_stock_picking_wave_done",
+        col1="type_id",
+        col2="group_id",
+    )
+
+    cancel_group_ids = fields.Many2many(
+        string="Allowed to Cancel",
+        comodel_name="res.groups",
+        rel="rel_stock_picking_wave_done",
+        col1="type_id",
+        col2="group_id",
+    )
