2458c8b0d99dc775d96915d37e7f00ed3d99f80b
Andhitia Rama <andhitia.r@gmail.com>
2018-09-17 23:46:56 +0700

    [8.0.1.0.0] stock_move_default_procure_method

---
+++ stock_move_default_procure_method/models/stock_picking_type.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    default_move_procure_method = fields.Selection(
+        string="Default Procure Method",
+        selection=[
+            ("make_to_stock", "Default: Take From Stock"),
+            ("make_to_order", "Advanced: Apply Procurement Rules")
+        ],
+    )
