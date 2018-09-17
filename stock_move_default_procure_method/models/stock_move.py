07445a387aa1cf224893b598305c8e212af3a3c0
Andhitia Rama <andhitia.r@gmail.com>
2018-09-17 23:46:56 +0700

    [8.0.1.0.0] stock_move_default_procure_method

---
+++ stock_move_default_procure_method/models/stock_move.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    @api.onchange("picking_type_id")
+    def onchange_procure_method(self):
+        picking_type = self.picking_type_id or False
+        if picking_type:
+            self.procure_method = picking_type.default_move_procure_method or \
+                "make_to_stock"
