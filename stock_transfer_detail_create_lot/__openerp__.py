8a18b9d727c3614a8b6066a1234bdf5f85cc764e
Andhitia Rama <andhitia.r@gmail.com>
2018-09-26 00:18:40 +0700

    [8.0.1.0.0] stock_transfer_detail_create_lot

---
+++ stock_transfer_detail_create_lot/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Auto Create Serial Number From Transfer Detail",
+    "version": "8.0.1.0.0",
+    "category": "Stock Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock_production_lot_by_product",
+    ],
+    "data": [
+        "wizards/stock_transfer_details.xml",
+    ],
+}
---
+++ stock_transfer_detail_create_lot/wizards/stock_transfer_details.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+
+
+class StockTransferDetails(models.TransientModel):
+    _inherit = "stock.transfer_details"
+
+    @api.multi
+    def button_generate_lot(self):
+        self.ensure_one()
+        for detail in self.item_ids:
+            detail._generate_lot()
+        return self.wizard_view()
+
+
+class StockTransferDetailsItems(models.TransientModel):
+    _inherit = "stock.transfer_details_items"
+
+    @api.multi
+    def _generate_lot(self):
+        self.ensure_one()
+        obj_lot = self.env["stock.production.lot"]
+        lot = obj_lot.create(self._prepare_generate_lot())
+        self.write({"lot_id": lot.id})
+
+    @api.multi
+    def _prepare_generate_lot(self):
+        return {
+            "name": "/",
+            "product_id": self.product_id.id,
+        }
