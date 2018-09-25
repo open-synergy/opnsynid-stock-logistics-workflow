8dd64521fa3ac722933aba219c0c87bcb2f994d9
Andhitia Rama <andhitia.r@gmail.com>
2018-09-25 23:27:49 +0700

    [8.0.1.0.0] stock_production_lot_by_product

---
+++ stock_production_lot_by_product/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Production Lot By Product",
+    "version": "8.0.1.0.0",
+    "category": "Stock Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/product_product_views.xml",
+    ],
+}
