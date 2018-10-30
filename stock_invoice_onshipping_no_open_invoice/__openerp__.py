5c8318fd3e2a920ee59bf8f4f2edacc131f4d686
Andhitia Rama <andhitia.r@gmail.com>
2018-10-30 11:35:54 +0700

    [8.0.1.0.0] stock_invoice_onshipping_no_open_invoice

---
+++ stock_invoice_onshipping_no_open_invoice/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Do not open invoice view when invoice onshipping",
+    "version": "8.0.1.0.0",
+    "category": "Stock Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock_account",
+    ],
+    "data": [
+    ],
+}
