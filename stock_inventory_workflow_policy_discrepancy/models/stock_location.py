73c49234f4bb453b63e8f54c183f0d860162abc3
Andhitia Rama <andhitia.r@gmail.com>
2018-11-11 20:11:44 +0700

    [8.0.1.0.0] stock_inventory_workflow_policy_discrepancy

---
+++ stock_inventory_workflow_policy_discrepancy/models/stock_location.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+
+
+class StockLocation(models.Model):
+    _inherit = "stock.location"
+
+    inventory_adjustment_force_grp_ids = fields.Many2many(
+        string="Allowed To Force Validation Inventory Adjustment",
+        comodel_name="res.groups",
+        relation="rel_inventory_adjustment_allowed_force_groups",
+        column1="location_id",
+        column2="group_id",
+    )
+
+    @api.model
+    def _get_inventory_adjustment_button_policy_map(self):
+        _super = super(StockLocation, self)
+        result = _super._get_inventory_adjustment_button_policy_map()
+        result.append(
+            ("force_ok", "inventory_adjustment_force_grp_ids")
+        )
+        return result
