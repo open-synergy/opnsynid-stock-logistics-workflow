0e14e5a090d74c7403760e07cf7931cac90a34f6
Andhitia Rama <andhitia.r@gmail.com>
2019-01-27 20:35:05 +0700

    [8.0.1.0.0] stock_picking_unexpected_product_transfer_policy

---
+++ stock_picking_unexpected_product_transfer_policy/models/stock_picking_type.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    allow_unexpected_product_transfer = fields.Boolean(
+        string="Allow Unexpected Product Transfer",
+        default=True,
+    )
