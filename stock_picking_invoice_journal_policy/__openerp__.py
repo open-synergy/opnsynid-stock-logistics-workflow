2022da821fc811765489cb726e3641c41a3df4da
Andhitia Rama <andhitia.r@gmail.com>
2019-01-27 19:35:59 +0700

    [8.0.1.0.0] stock_picking_invoice_journal_policy

---
+++ stock_picking_invoice_journal_policy/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Create Invoice From Picking's Journal Policy",
+    "version": "8.0.1.0.0",
+    "category": "Stock Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock_account",
+    ],
+    "data": [
+        "wizards/stock_invoice_onshipping.xml",
+        "views/stock_picking_type_views.xml",
+    ],
+}
---
+++ stock_picking_invoice_journal_policy/wizards/stock_invoice_onshipping.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api, fields
+
+
+class StockInvoiceOnshipping(models.TransientModel):
+    _inherit = "stock.invoice.onshipping"
+
+    @api.model
+    def _default_stock_picking_id(self):
+        active_ids = self._context.get("active_ids", [])
+        if len(active_ids) > 0:
+            return active_ids[0]
+        else:
+            return False
+
+    stock_picking_id = fields.Many2one(
+        string="Stock Picking",
+        comodel_name="stock.picking",
+        default=lambda self: self._default_stock_picking_id(),
+    )
+    allowed_invoice_journal_ids = fields.Many2many(
+        string="Allowed Invoice Journals",
+        comodel_name="account.journal",
+        related="stock_picking_id.picking_type_id.allowed_invoice_journal_ids",
+    )
+
+    @api.onchange("stock_picking_id")
+    def onchange_journal_id(self):
+        self.journal_id = False
+        if self.stock_picking_id:
+            picking = self.stock_picking_id
+            if picking.picking_type_id:
+                picking_type = picking.picking_type_id
+                if picking_type.default_invoice_journal_id:
+                    self.journal_id = picking_type.default_invoice_journal_id
