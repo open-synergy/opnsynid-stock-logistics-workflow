2925dcaf5d862042dbe01b99a616084618bdcbb0
mikevhe18 <viriyananda.michael@gmail.com>
2019-06-11 14:53:36 +0700

    [8.0.2.0.0][stock_picking_print_policy]

--- stock_picking_print_policy/__init__.py
+++ stock_picking_print_policy/__init__.py
@@ -2,4 +2,3 @@
 # Copyright 2017 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import models
--- stock_picking_print_policy/__openerp__.py
+++ stock_picking_print_policy/__openerp__.py
@@ -3,18 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Picking Print Policy",
-    "version": "8.0.1.1.0",
+    "version": "8.0.2.0.0",
     "category": "Stock Management",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "stock_picking_policy",
-        "stock_operation_type_create_menu",
+        "stock",
+        "base_print_policy"
     ],
     "data": [
-        "security/ir.model.access.csv",
-        "views/stock_picking_type_views.xml",
+        "views/stock_picking_views.xml"
     ],
 }
