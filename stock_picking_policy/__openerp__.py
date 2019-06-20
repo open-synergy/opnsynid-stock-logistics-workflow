20fbf13208624d8dec0af58364abdc73529065a4
mikevhe18 <viriyananda.michael@gmail.com>
2019-06-21 00:47:31 +0700

    [8.0.2.0.0][stock_picking_policy]

--- stock_picking_policy/__openerp__.py
+++ stock_picking_policy/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Picking Policy",
-    "version": "8.0.1.0.1",
+    "version": "8.0.2.0.0",
     "category": "Stock Management",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
@@ -11,8 +11,10 @@
     "installable": True,
     "depends": [
         "stock",
+        "base_workflow_policy",
     ],
     "data": [
+        "data/base_workflow_policy_data.xml",
         "views/stock_picking_type_views.xml",
         "views/stock_picking_views.xml",
     ],
