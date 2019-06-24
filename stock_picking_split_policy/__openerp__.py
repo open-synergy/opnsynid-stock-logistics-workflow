44fd1abf0835a5e05aae5fddb61dd2049ca1f125
mikevhe18 <viriyananda.michael@gmail.com>
2019-06-24 09:35:30 +0700

    [8.0.2.0.0][stock_picking_split_policy]

--- stock_picking_split_policy/__openerp__.py
+++ stock_picking_split_policy/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Picking Split Policy",
-    "version": "8.0.1.1.0",
+    "version": "8.0.2.0.0",
     "category": "Stock Management",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
@@ -14,6 +14,7 @@
         "stock_split_picking",
     ],
     "data": [
+        "data/base_workflow_policy_data.xml",
         "views/stock_picking_type_views.xml",
         "views/stock_picking_views.xml",
     ],
