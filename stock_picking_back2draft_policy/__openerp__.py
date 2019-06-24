240b92e44194d0d037445c440178c6ac9b6f444e
mikevhe18 <viriyananda.michael@gmail.com>
2019-06-24 09:25:33 +0700

    [8.0.2.0.0][stock_picking_back2draft_policy]

--- stock_picking_back2draft_policy/__openerp__.py
+++ stock_picking_back2draft_policy/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Picking Back To Draft Policy",
-    "version": "8.0.1.1.0",
+    "version": "8.0.2.0.0",
     "category": "Stock Management",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
@@ -14,6 +14,7 @@
         "stock_picking_back2draft",
     ],
     "data": [
+        "data/base_workflow_policy_data.xml",
         "views/stock_picking_type_views.xml",
         "views/stock_picking_views.xml",
     ],
--- stock_picking_back2draft_policy/models/stock_picking.py
+++ stock_picking_back2draft_policy/models/stock_picking.py
@@ -10,31 +10,11 @@ class StockPicking(models.Model):
 
     @api.multi
     @api.depends(
-        "picking_type_id.back2draft_group_ids",
+        "picking_type_id",
     )
     def _compute_policy(self):
-        super(StockPicking, self)._compute_policy()
-        obj_picking_type = self.env["stock.picking.type"]
-        for picking in self:
-            picking.back2draft_ok = False
-            picking_id = self.env.context.get("default_picking_type_id", False)
-            if not picking_id:
-                continue
-            picking_type = obj_picking_type.browse([picking_id])[0]
-            picking.back2draft_ok = self._back2draft_policy(picking_type)
-
-    @api.model
-    def _back2draft_policy(self, picking_type):
-        result = False
-        user = self.env.user
-        back2draft_group_ids = picking_type.back2draft_group_ids.ids
-        group_ids = user.groups_id.ids
-        if not picking_type.back2draft_group_ids.ids:
-            result = True
-        else:
-            if (set(back2draft_group_ids) & set(group_ids)):
-                result = True
-        return result
+        _super = super(StockPicking, self)
+        _super._compute_policy()
 
     back2draft_ok = fields.Boolean(
         string="Can Back to Draft",
