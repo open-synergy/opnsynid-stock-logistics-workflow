1d44a91f4593cf2e5fe085f193d6a8583f90bf97
mikevhe18 <viriyananda.michael@gmail.com>
2021-01-18 11:22:34 +0700

    [stock_move_backdating] 14.0.1.0.0

---
+++ stock_move_backdating/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2015-2016 Agile Business Group (<http://www.agilebg.com>)
+# Copyright 2015 BREMSKERL-REIBBELAGWERKE EMMERLING GmbH & Co. KG
+#    Author Marco Dieckhoff
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Stock Move Backdating",
+    "version": "12.0.1.0.0",
+    "category": "Stock Logistics",
+    "author": "Marco Dieckhoff, BREMSKERL, Agile Business Group, "
+    "Odoo Community Association (OCA), "
+    "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "website": "https://simetri-sinergi.id",
+    "depends": [
+        "stock_account",
+    ],
+    "data": [
+        "views/stock_picking_view.xml",
+    ],
+    "installable": True,
+}
