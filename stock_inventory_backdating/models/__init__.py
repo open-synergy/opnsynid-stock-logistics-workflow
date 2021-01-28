d797572dc11729e058a0fe6671963f441fab6d31
mikevhe18 <viriyananda.michael@gmail.com>
2021-01-28 10:33:44 +0700

    [stock_inventory_backdating] 12.0.1.0.0

--- stock_inventory_backdating/models/__init__.py
+++ stock_inventory_backdating/models/__init__.py
@@ -1,5 +1,8 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 OpenSynergy Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import stock_inventory
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from . import (
+    stock_move,
+    stock_inventory,
+    account_move,
+)
