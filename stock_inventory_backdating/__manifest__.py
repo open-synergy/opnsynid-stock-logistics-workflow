e5f755c3249dd4dbc57d8f868d8b97e66dc31a52
mikevhe18 <viriyananda.michael@gmail.com>
2021-02-01 10:18:14 +0700

    [stock_inventory_backdating] 12.0.1.0.1

--- stock_inventory_backdating/__manifest__.py
+++ stock_inventory_backdating/__manifest__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Stock Inventory Backdating",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "category": "Accounting & Finance",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
