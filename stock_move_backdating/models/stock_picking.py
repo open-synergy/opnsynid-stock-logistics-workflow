aa9b41e0aecc121c2efde4a9121499cfa724b8b8
Andhitia Rama <andhitia.r@gmail.com>
2023-12-23 05:51:41 +0700

    [UPD] stock_move_backdating
    * Actual movement date can be input on picking

--- stock_move_backdating/models/stock_picking.py
+++ stock_move_backdating/models/stock_picking.py
@@ -2,11 +2,37 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
+
+    date_backdating = fields.Datetime(
+        string="Actual Movement Date",
+        compute="_compute_date_backdating",
+        inverse="_set_move_lines_date_backdating",
+        store=True,
+        readonly=True,
+        states={
+            "assigned": [
+                ("readonly", False),
+            ],
+        },
+    )    
+
+    @api.depends(
+        "move_lines",
+        "move_lines.date_backdating",
+    )
+    def _compute_date_backdating(self):
+        for record in self:
+            if record.move_lines:
+                record.date_backdating = record.move_lines[0].date_backdating
+
+    def _set_move_lines_date_backdating(self):
+        for picking in self:
+            picking.move_lines.write({'date_backdating': picking.date_backdating})                
 
     def action_done(self):
         _super = super(StockPicking, self)
